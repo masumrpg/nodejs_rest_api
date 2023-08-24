1faef78205b06926ada91ca362c8718961d1ad7e
masumrpg <masumrpg@gmail.com>
2023-08-24 17:07:19 +0700

    login clear

--- test/user.test.js
+++ test/user.test.js
@@ -95,4 +95,46 @@ describe('POST /api/users/login', () => {
         expect(result.body.data.token).toBeDefined();
         expect(result.body.data.token).not.toBe("test");
     });
+
+    it('should reject login if request is invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "",
+                password: ""
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should reject login if password is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "test",
+                password: "salah"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should reject login if username is wrong', async () => {
+        const result = await supertest(web)
+            .post('/api/users/login')
+            .send({
+                username: "salah",
+                password: "salah"
+            });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
 })
